45cd2894d41fadaa9ed8d78322b2ac65bb534423
juppelis <jussi.alhanen@omnia.fi>
2024-03-13 18:05:04 +0200

    työ alkuun
    Aloituskoodit

---
+++ testi.py
@@ -0,0 +1,12 @@
+sana=input("kerro merkkijono: ")
+haettavasana=input("mitä sanaa haetaan: ")
+indeksi=sana.find(haettavasana)
+if indeksi !=-1:
+    print(indeksi)
+    print(sana[indeksi:indeksi+len(haettavasana)])
+else:
+    print("haettavaa sanaa ei löytynyt")
+
+
+sana[aloituskohta:lopetuskohta]
+sana[3]
---
+++ tulostustesti.py
@@ -0,0 +1,13 @@
+print("Moikka maailma. Mitä kuuluu?")
+print("Mitä kuuluu?")
+#Tämä on kommentti
+#print((4+4)*10)
+#Tässä kysytään nimeä
+nimi=input("Anna nimesi: ")
+print("Tervepä terve", nimi)
+toinenNimi=input("Anna nimesi: ")
+print("Sinun nimesi on siis "+toinenNimi+", eikös vaan?")
+kolmasnimi="Hannu Hanhi"
+nimi="laksjflksajf"
+print("kolmasnimi on siis "+kolmasnimi+" ja nimeen tuli teksti "+nimi)
+
